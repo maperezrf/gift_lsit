b022f4db68d8b88f5814323a6a7693beb31789be
miguel perez <maperez.r@hotmail.com>
2026-01-17 10:09:04 -0500

    requeriemnts

--- gifts_list/settings.py
+++ gifts_list/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
+print(DEBUG)
 
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
